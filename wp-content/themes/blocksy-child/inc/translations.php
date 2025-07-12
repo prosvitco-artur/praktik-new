97b8c4d6631d036935d7c90e34d604e80f6988b0
artur <artur.babii@alkima.org>
2025-07-12 21:55:50 +0300

    delete blocksy

--- wp-content/themes/blocksy-child/inc/translations.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Load theme translations
- */
-function praktik_load_theme_textdomain() {
-	load_theme_textdomain( 'praktik', get_template_directory() . '/languages' );
-}
-add_action( 'after_setup_theme', 'praktik_load_theme_textdomain' ); 
