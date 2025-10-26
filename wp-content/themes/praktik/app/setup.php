86ecb4f7d609704110146e767a6077c2ff4028cd
artur <artur.babii@alkima.org>
2025-10-26 16:28:33 +0200

    Add svg support

--- wp-content/themes/praktik/app/setup.php
+++ wp-content/themes/praktik/app/setup.php
@@ -156,3 +156,17 @@ add_filter('template_include', function ($template) {
 add_action('after_switch_theme', function () {
     flush_rewrite_rules();
 });
+
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
+
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+    $filetype = wp_check_filetype($filename, $mimes);
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+}, 10, 4);
