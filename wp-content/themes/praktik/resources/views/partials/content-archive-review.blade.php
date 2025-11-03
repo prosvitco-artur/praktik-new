bb5addab9ba78d437f121ad0324a310eb530acfe
artur <artur.babii@alkima.org>
2025-11-03 15:21:18 +0200

    Update pre review

--- wp-content/themes/praktik/resources/views/partials/content-archive-review.blade.php
+++ wp-content/themes/praktik/resources/views/partials/content-archive-review.blade.php
@@ -1,21 +1,21 @@
 <article @php(post_class('bg-white mb-5 overflow-hidden review-card'))>
   <div class="flex items-start">
-    <div class="">
+    <div class="review-image">
       @if (has_post_thumbnail())
         {!! get_the_post_thumbnail(get_the_ID()) !!}
       @endif
     </div>
     <div class="p-3 md:p-5">
       <div class="mb-3">
-        <p class="font-bold mb-1 text-[14px]" style="line-height: 18px;">
+        <p class="font-bold mb-1 review-title">
           {!! get_the_title() !!}
         </p>
-        <p class="text-neutral-900 text-[12px]" style="line-height: 16px;">
+        <p class="text-neutral-900 review-date">
           {{ get_the_date('d.m.Y') }}
         </p>
       </div>
 
-      <div class="text-[12px] review-content line-clamp-4">
+      <div class="review-content line-clamp-4">
         {!! get_the_content() !!}
       </div>
     </div>
