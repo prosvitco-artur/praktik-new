04645052da63f89327805882a0ab6d87b21b2017
prosvitco-artur <arturbabii08@gmail.com>
2025-11-09 23:40:50 +0200

    Property Styles

--- wp-content/themes/praktik/resources/views/partials/content-single-property.blade.php
+++ wp-content/themes/praktik/resources/views/partials/content-single-property.blade.php
@@ -23,79 +23,79 @@
 
   <div class="container mx-auto mb-10">
     <div class="property-single">
-        <div class="property-gallery">
-          {{-- Main Swiper --}}
-          <div class="swiper property-gallery-main" style="aspect-ratio: 3/2;">
-            <div class="swiper-wrapper">
-              @if(!empty($property_gallery))
-                @foreach($property_gallery as $index => $image)
-                  <div class="swiper-slide">
-                    <a href="{{ $image['url'] }}" data-fancybox="property-gallery"
-                      data-caption="{{ $image['alt'] ?: $image['title'] ?: get_the_title() }}" class="block w-full h-full">
-                      <img src="{{ $image['url'] }}" alt="{{ $image['alt'] ?: $image['title'] ?: get_the_title() }}"
-                        class="w-full h-full object-cover cursor-pointer">
-                    </a>
-                  </div>
-                @endforeach
-              @elseif(has_post_thumbnail())
+      <div class="property-gallery">
+        {{-- Main Swiper --}}
+        <div class="swiper property-gallery-main" style="aspect-ratio: 3/2;">
+          <div class="swiper-wrapper">
+            @if(!empty($property_gallery))
+              @foreach($property_gallery as $index => $image)
                 <div class="swiper-slide">
-                  <a href="{{ get_the_post_thumbnail_url($post_id, 'full') }}" data-fancybox="property-gallery"
-                    data-caption="{{ get_the_title() }}" class="block w-full h-full">
-                    <img src="{{ get_the_post_thumbnail_url($post_id, 'large') }}" alt="{{ get_the_title() }}"
+                  <a href="{{ $image['url'] }}" data-fancybox="property-gallery"
+                    data-caption="{{ $image['alt'] ?: $image['title'] ?: get_the_title() }}" class="block w-full h-full">
+                    <img src="{{ $image['url'] }}" alt="{{ $image['alt'] ?: $image['title'] ?: get_the_title() }}"
                       class="w-full h-full object-cover cursor-pointer">
                   </a>
                 </div>
-              @else
-                <div class="swiper-slide">
-                  <div class="w-full h-full flex items-center justify-center text-gray-500">
-                    {{ __('No image available', 'praktik') }}
-                  </div>
+              @endforeach
+            @elseif(has_post_thumbnail())
+              <div class="swiper-slide">
+                <a href="{{ get_the_post_thumbnail_url($post_id, 'full') }}" data-fancybox="property-gallery"
+                  data-caption="{{ get_the_title() }}" class="block w-full h-full">
+                  <img src="{{ get_the_post_thumbnail_url($post_id, 'large') }}" alt="{{ get_the_title() }}"
+                    class="w-full h-full object-cover cursor-pointer">
+                </a>
+              </div>
+            @else
+              <div class="swiper-slide">
+                <div class="w-full h-full flex items-center justify-center text-gray-500">
+                  {{ __('No image available', 'praktik') }}
                 </div>
-              @endif
-            </div>
-
-            {{-- Navigation buttons --}}
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-
-            {{-- Photo counter --}}
-            <div
-              class="absolute bottom-8px right-8px bg-black bg-opacity-60 text-white px-8px py-4px text-caption rounded-4px font-semibold z-10">
-              {{ __('PHOTO', 'praktik') }} <span
-                class="property-photo-counter">1/{{ $gallery_count ?: ($property_meta['photos_count'] ?? 1) }}</span>
-            </div>
-
-            {{-- Pagination --}}
-            <div class="swiper-pagination"></div>
+              </div>
+            @endif
           </div>
 
-          {{-- Thumbnail Navigation Swiper --}}
-          @if($gallery_count > 1)
-            <div class="swiper property-gallery-thumbs mt-4" style="height: auto;">
-              <div class="swiper-wrapper">
-                @foreach($property_gallery as $image)
-                  <div class="swiper-slide" style="aspect-ratio: 4/3;">
-                    <img src="{{ $image['thumbnail'] }}" alt="{{ $image['alt'] ?: $image['title'] }}"
-                      class="w-full h-full object-cover">
-                  </div>
-                @endforeach
-              </div>
-            </div>
-          @endif
+          {{-- Navigation buttons --}}
+          <div class="swiper-button-next"></div>
+          <div class="swiper-button-prev"></div>
+
+          {{-- Photo counter --}}
+          <div
+            class="absolute bottom-8px right-8px bg-black bg-opacity-60 text-white px-8px py-4px text-caption rounded-4px font-semibold z-10">
+            {{ __('PHOTO', 'praktik') }} <span
+              class="property-photo-counter">1/{{ $gallery_count ?: ($property_meta['photos_count'] ?? 1) }}</span>
+          </div>
+
+          {{-- Pagination --}}
+          <div class="swiper-pagination"></div>
         </div>
+
+        {{-- Thumbnail Navigation Swiper --}}
+        @if($gallery_count > 1)
+          <div class="swiper property-gallery-thumbs mt-4" style="height: auto;">
+            <div class="swiper-wrapper">
+              @foreach($property_gallery as $image)
+                <div class="swiper-slide" style="aspect-ratio: 4/3;">
+                  <img src="{{ $image['thumbnail'] }}" alt="{{ $image['alt'] ?: $image['title'] }}"
+                    class="w-full h-full object-cover">
+                </div>
+              @endforeach
+            </div>
+          </div>
+        @endif
+      </div>
       <div class="property-details container px-4 md:px-0">
         {{-- Title and actions --}}
         <div class="md:bg-white md:p-10 mb-6">
           <div class="md:flex md:items-start md:justify-between ">
             <div class="flex-1 mb-5">
-              <h1 class="text-gray-900 mb-2 text-2xl">
+              <h1 class="property-title">
                 {{ get_the_title() ?: __('No Title', 'praktik') }}
               </h1>
-              <div class="text-sm text-gray-600 mb-3">
+              <div class="text-xs text-neutral-600 mb-3">
                 {{ __('ID', 'praktik') }} {{ $post_id ?: __('Unknown ID', 'praktik') }}
               </div>
               @if(!empty($property_meta['price']))
-                <div class="text-[24px] leading-[130%] md:text-[32px] font-bold text-secondary-500">
+                <div class="text-[24px] leading-[130%] md:text-[32px] font-bold text-primary-500">
                   {{ format_property_price($property_meta['price']) }}
                 </div>
               @endif
@@ -152,5 +152,14 @@
 
         <x-agent-contact />
       </div>
+      @if(get_the_content())
+      <div class="property-description container px-4 md:px-0">
+        <div class="text-h4 text-gray-900 mb-4">{{ __('Property Description', 'praktik') }}</div>
+        <div class="property-content text-p1 text-gray-700 leading-relaxed">
+          @php(the_content())
+        </div>
+      </div>
+      @endif
     </div>
+  </div>
 </article>
