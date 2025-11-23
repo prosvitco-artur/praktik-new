e311eac2fb800665b5c1341ad0c0d5d928a6d6cd
Artur Babii <artur.babii@alkima.org>
2025-11-23 23:32:40 +0200

    Author Data

--- wp-content/themes/praktik/resources/views/components/agent-contact.blade.php
+++ wp-content/themes/praktik/resources/views/components/agent-contact.blade.php
@@ -1,16 +1,21 @@
 @php
 $authorId = get_post_field('post_author', get_the_ID());
 $name = get_the_author_meta('display_name', $authorId) ?: '';
-$rawPhone = get_user_meta($authorId, 'phone', true) ?: get_user_meta($authorId, 'user_phone', true) ?: '';
+$rawPhone = carbon_get_user_meta($authorId, 'author_phone') ?: '';
 $phoneHref = $rawPhone ? 'tel:' . preg_replace('/[^\d\+]/', '', $rawPhone) : '';
-$telegramHref = get_user_meta($authorId, 'telegram', true);
+$telegramHref = carbon_get_user_meta($authorId, 'author_telegram') ?: '';
+$authorPhoto = carbon_get_user_meta($authorId, 'author_photo') ?: '';
 $telegramLabel = __('Write to Telegram', 'praktik');
 @endphp
 
 <div class="fixed bottom-0 left-0 right-0 bg-white p-5 flex items-center justify-between md:p-10 md:static">
   <div class="flex items-center gap-4">
     <div class="rounded-full h-12 w-12 overflow-hidden">
-      {!! get_avatar($authorId, 96) !!}
+      @if($authorPhoto)
+        <img src="{{ $authorPhoto }}" alt="{{ $name }}" class="w-full h-full object-cover">
+      @else
+        {!! get_avatar($authorId, 96) !!}
+      @endif
     </div>
     <div>
       <p class="font-bold">{{ $name }}</p>
@@ -19,12 +24,14 @@ $telegramLabel = __('Write to Telegram', 'praktik');
       @endif
     </div>
   </div>
-  @if (!empty($telegramHref))
-  <a href="{{ $telegramHref }}" class="md:flex md:items-center md:gap-2 md:justify-center md:border md:border-secondary-500 md:py-2 md:px-4">
-    <x-icon name="telegram-message" class="w-6 h-6 text-secondary-500" />
-    <span class="text-secondary-500 hidden md:block">{{ $telegramLabel }}</span>
-  </a>
-  @endif
+  <div class="flex items-center gap-2">
+    @if (!empty($telegramHref))
+    <a href="{{ $telegramHref }}" target="_blank" rel="noopener noreferrer" class="md:flex md:items-center md:gap-2 md:justify-center md:border md:border-secondary-500 md:py-2 md:px-4">
+      <x-icon name="telegram-message" class="w-6 h-6 text-secondary-500" />
+      <span class="text-secondary-500 hidden md:block">{{ $telegramLabel }}</span>
+    </a>
+    @endif
+  </div>
 </div>
 
 
